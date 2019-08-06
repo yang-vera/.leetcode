da01c4c1ec651aa4dea8634fb153f05d67ab6933
Yang Zhao <Yang.Zhao@sas.com>
2019-08-06 18:06:56 -0400

    test

--- 57.insert-interval.py
+++ 57.insert-interval.py
@@ -5,5 +5,19 @@
 #
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        
+        i = 0
+        length = len(intervals)
+        new_l = newInterval[0]
+        new_r = newInterval[1]
+        left=[]
+        right=[]
+        for num_list in intervals:
+            if num_list[0]> new_r:
+                right.append(num_list)
+            elif num_list[1] < new_l:
+                left.append(num_list)
+            else:
+                new_l=min(new_l, num_list[0])
+                new_r=max(new_r, num_list[1])
+        return left + [[new_l, new_r]] + right
 
