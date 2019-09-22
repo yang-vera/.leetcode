523766990eabbf7a3ea6ce601f5232c18236838d
Yang <joyzhyang@hotmail.com>
2019-09-21 21:59:31 -0400

    add

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -40,5 +40,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left = i+1
+            right = len(nums)-1
+            while left < right:
+                sum = nums[left] + nums[right]
+                if sum + nums[i] > 0:
+                    right-=1
+                #here elif is needed. parallel condition
+                elif sum + nums[i] < 0:
+                    left+=1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                    #if sum is zero, move left and right together
+                    #left and right should not equal to their original values
+                    left+=1
+                    right-=1
+                    while left < right and nums[left]==nums[left-1]:
+                        left+=1
+                    while left < right and nums[right]==nums[right+1]:
+                        right-=1
+        return res
+                
 
