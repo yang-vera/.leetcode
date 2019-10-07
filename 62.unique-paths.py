78f6310452ac53c9ec0b73cb4c8047f39cdd1b89
Yang <joyzhyang@hotmail.com>
2019-10-07 01:05:25 -0400

    add

--- 62.unique-paths.py
+++ 62.unique-paths.py
@@ -5,8 +5,14 @@
 #
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-
+        '''
         return int(math.factorial(m+n-2)/math.factorial(m-1)/math.factorial(n-1))
-
-        
+        '''
+        if m==1 or n==1:
+            return 1
+        dp = [1]*m
+        for i in range(1,n):
+            for j in range(1,m):
+                dp[j]=dp[j]+dp[j-1]
+        return dp[-1]
 
